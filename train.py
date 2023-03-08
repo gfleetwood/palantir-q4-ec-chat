84df3a3dedaf6e6a51849f645de022ab489c0e4d
gfleetwood <gfleetwood@protonmail.com>
2023-03-08 07:05:22 +0000

    removed import bloat

--- train.py
+++ train.py
@@ -1,12 +1,5 @@
-from io import StringIO
-import sys
-from typing import Dict, Optional
-
-from langchain.agents import load_tools
-from langchain.agents import initialize_agent
-from langchain.agents.tools import Tool
 from langchain.llms import OpenAI
-from langchain.document_loaders import DirectoryLoader, UnstructuredFileLoader, UnstructuredPDFLoader, OnlinePDFLoader, UnstructuredHTMLLoader
+from langchain.document_loaders import UnstructuredHTMLLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
